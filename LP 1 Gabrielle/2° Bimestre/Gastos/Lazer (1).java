b7a69c970e9c91aaf6b29c29d8e940ecf61c7dd2
Victória <126773570+GabiVictoria@users.noreply.github.com>
2023-05-25 08:30:51 -0300

    Add files via upload

---
+++ LP 1 Gabrielle/2° Bimestre/Gastos/Lazer (1).java
@@ -0,0 +1,22 @@
+public class Lazer extends Gastos {
+    private boolean importante;
+
+    Lazer (String nome, double valor, boolean importante) {
+        super(nome, valor);
+        this.importante = importante;
+    }
+    public boolean getImportante(){
+        return importante;
+    }
+    public void setImportante(boolean importante){
+        this.importante = importante;
+    }
+    
+    public void mudarValor(double valor){
+        super.mudarValor(valor * 1.05);
+    }
+    
+    public String apresentarTudo(){
+        return super.apresentarTudo() + " // Importante: " + (getImportante() ? "sim" : "não");
+    }
+}
