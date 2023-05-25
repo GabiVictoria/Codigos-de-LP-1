849e337cdfc6296955641077e3d383a47fdff648
Victória <126773570+GabiVictoria@users.noreply.github.com>
2023-05-25 08:46:48 -0300

    Delete LP 1 Gabrielle directory

--- LP 1 Gabrielle/2° Bimestre/Gastos/Fixo (1).java
+++
@@ -1,8 +0,0 @@
-public class Fixo extends Gastos{
-    Fixo(String nome, double valor){
-        super(nome, valor);
-    }
-    public String apresentarTudo(){
-        return super.apresentarTudo();
-    }
-}
