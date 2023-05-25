1550db8d1dd704ceeaff33c2191e6acf646eb030
Victória <126773570+GabiVictoria@users.noreply.github.com>
2023-05-25 08:48:28 -0300

    Add files via upload

---
+++ LP 1 Gabrielle/2° Bimestre/biblioteca/src/livros.java
@@ -0,0 +1,35 @@
+public class livros {
+    private String nome;
+    private String autor;
+    private int ano;
+
+    public livros(String nome, String autor, int ano) {
+        this.nome = nome;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
