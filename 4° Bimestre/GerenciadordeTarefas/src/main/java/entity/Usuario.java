b5b9e7446d5ca271a050462277247a7e4d502c54
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-10-15 22:04:15 -0300

    Update Usuario.java
    Foi comentado o relacionamento dos atributos e a função da classe.

--- 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Usuario.java
+++ 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Usuario.java
@@ -6,6 +6,7 @@ import java.util.List;
 @Entity(name = "Usuario")
 @Table(name = "Usuario")
 
+//A classe Usuário gera objetos que contém as informações referentes aos usuários do sistema gerenciador de tarefas
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,21 +17,29 @@ public class Usuario {
     private String login;
     @Column
     private String senha;
+    //indica um relacionamento "um para muitos" com a classe Usuario:
     @OneToMany
+    //indica que a classe vai ter uma chave estrangeira da classe Usuario:
     @JoinColumn(name="usuario_id")
     protected static List<Tarefa> tarefas= new ArrayList<>();
+    //indica um relacionamento "um para muitos" com a classe Usuario:
     @OneToMany
+    //indica que a classe Materia possui uma chave estrangeira da classe Usuario:
     @JoinColumn(name="usuario_id")
     protected static List<Materia> materias= new ArrayList<>();
+    //indica um relacionamento "um para muitos" com a classe Usuario:
     @OneToMany
+    //indica que a classe Categoria possui uma chave estrangeira da classe Usuario:
     @JoinColumn(name="usuario_id")
     protected static List<Categoria> categorias= new ArrayList<>();
+    //indica um relacionamento "um para muitos" com a classe Usuario:
     @OneToMany
+    //indica que a classe Notificacao possui uma chave estrangeira da classe Usuario:
     @JoinColumn(name="usuario_id")
     protected static List<Notificacao> notificacoes= new ArrayList<>();
 
 
-
+    
     public void  tarefaPendente(){
         for (Tarefa p:Usuario.tarefas) {
             if(p.getStatus().equals("Pendente")){
