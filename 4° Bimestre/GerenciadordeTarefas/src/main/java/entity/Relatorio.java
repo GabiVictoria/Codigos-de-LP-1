c1085bfeacc7c101bfa39300c7fdca14684f549d
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-10-15 22:09:18 -0300

    Update Relatorio.java

--- 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Relatorio.java
+++ 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Relatorio.java
@@ -2,6 +2,8 @@ package entity;
 import jakarta.persistence.*;
 @Entity(name = "Materia")
 @Table(name = "Materia")
+    
+//A classe Relatorio relata o progresso das tarefas
 public class Relatorio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
