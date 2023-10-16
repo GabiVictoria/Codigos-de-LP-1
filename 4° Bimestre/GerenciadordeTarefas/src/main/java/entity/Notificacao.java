a8fe43f78b287c2eb02ae376745ea4a0ea502cf7
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-10-15 22:05:17 -0300

    Update Notificacao.java

--- 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Notificacao.java
+++ 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Notificacao.java
@@ -1,6 +1,7 @@
 package entity;
 import jakarta.persistence.*;
-/*A classe Notificacao é responsável por gerar os objetos referentes as notificações que o usuário pode receber sobre as tarefas que deve cumprir no determinado dia e horário*/
+/*A classe Notificacao é responsável por gerar os objetos referentes as notificações que o usuário pode receber sobre as tarefas que 
+deve cumprir no determinado dia e horário*/
 public class Notificacao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -10,11 +11,11 @@ public class Notificacao {
     @Column
     private String mensagem;
     @Column
-    /*indica um relacionamento "muitos para um" com a classe Tarefa:*/
+    //indica um relacionamento "muitos para um" com a classe Tarefa:
     @ManyToOne
     private Tarefa tarefa;
     @Column
-    /*indica um relacionamento "muitos para um" com a classe Categoria:*/
+    //indica um relacionamento "muitos para um" com a classe Categoria:
     @ManyToOne
     private Categoria categoria;
 }
