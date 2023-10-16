c8cde09106a87439d2ba5d319727892d699af1b5
SarahSLG <126773265+SarahSLG@users.noreply.github.com>
2023-10-15 21:36:45 -0300

    Update Notificacao.java
    Foi comentado a função da classe Notificacao e os relacionamentos dos atributos categoria e tarefa.

--- 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Notificacao.java
+++ 4° Bimestre/GerenciadordeTarefas/src/main/java/entity/Notificacao.java
@@ -1,5 +1,6 @@
 package entity;
 import jakarta.persistence.*;
+/*A classe Notificacao é responsável por gerar os objetos referentes as notificações que o usuário pode receber sobre as tarefas que deve cumprir no determinado dia e horário*/
 public class Notificacao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -9,9 +10,11 @@ public class Notificacao {
     @Column
     private String mensagem;
     @Column
+    /*indica um relacionamento "muitos para um" com a classe Tarefa:*/
     @ManyToOne
     private Tarefa tarefa;
     @Column
+    /*indica um relacionamento "muitos para um" com a classe Categoria:*/
     @ManyToOne
     private Categoria categoria;
 }
